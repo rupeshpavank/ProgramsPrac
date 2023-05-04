284e53e7c1295810fe27fd33c2319b40375353a3
kittu9231 <kittu9231@gmail.com>
2023-05-04 11:07:36 -0400

    changes

--- src/com/java/StringPractice/StringPractice.java
+++ src/com/java/StringPractice/StringPractice.java
@@ -244,6 +244,8 @@ public class StringPractice {
 		 * //Converts given string into character array char string[] =
 		 * string1.toCharArray();
 		 * 
+		 * 
+		 * 
 		 * System.out.println("Duplicate characters in a given string: "); //Counts each
 		 * character present in the string for(int i = 0; i <string.length; i++) { count
 		 * = 1; for(int j = i+1; j <string.length; j++) { if(string[i] == string[j] &&
